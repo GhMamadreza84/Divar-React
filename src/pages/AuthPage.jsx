a3e3016414d021ae4393c318cc2779f86facffcc
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-29 22:49:16 -0700

    show send otp form and check otp form in auth page

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+import CheckOtpForm from "../components/templates/CheckOtpForm";
+import SendOtpForm from "../components/templates/SendOtpForm";
 
 const AuthPage = () => {
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
   return (
     <div>
-      Auth Page
+      {step === 1 && <SendOtpForm />}
+      {step === 2 && <CheckOtpForm />}
     </div>
   );
-}
+};
 
 export default AuthPage;
