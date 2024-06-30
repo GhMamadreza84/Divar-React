322514f25b24d9bbe8545f87f26040fc32f40964
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-29 23:07:37 -0700

    add jsx and state in send otp component and use props

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,10 +1,28 @@
-
-const SendOtpForm = () => {
+const SendOtpForm = ({ mobile, setMobile, setStep }) => {
+  const submitHandler = (event) => {
+    event.preventDefault;
+    console.log(event);
+  };
   return (
     <div>
-      Send OTp
+      <form action="" onSubmit={submitHandler}>
+        <p>ورود به حساب کاربری</p>
+        <span>
+          برای استفاده از امکانات دیوار , لطفا شماره موبایل خود را وارد کنید .
+          کد تایید به این شماره پیامک خواهد شد .
+        </span>
+        <label htmlFor="input">شماره موبایل خود را وارد کنید</label>
+        <input
+          type="text"
+          id="input"
+          placeholder="شماره موبایل"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+        />
+        <button type="submit">ارسال کد تایید</button>
+      </form>
     </div>
   );
-}
+};
 
 export default SendOtpForm;
