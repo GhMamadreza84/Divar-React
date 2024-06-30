0b5b5362f06404ed2f7c704993aff3b6cad7a480
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-29 22:43:13 -0700

    edit app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import AuthPage from "./pages/AuthPage";
 
 function App() {
-  return(
-    <h1>پروژه دیوار</h1>
-    <AuthPage />
+  return (
+    <>
+      <h1>پروژه دیوار</h1>
+      <AuthPage />
+    </>
   );
-  
 }
 
 export default App;
