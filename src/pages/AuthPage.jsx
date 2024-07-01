352124c0aa271064f2544759edf59b57246359b3
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-30 22:52:06 -0700

    edit state un auth page

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,7 +3,7 @@ import CheckOtpForm from "../components/templates/CheckOtpForm";
 import SendOtpForm from "../components/templates/SendOtpForm";
 
 const AuthPage = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const [mobile, setMobile] = useState("");
   const [code, setCode] = useState("");
   return (
