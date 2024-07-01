dfb074bac33a0fc8b4c4d0af31a928419595de47
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-30 22:58:43 -0700

    add input for get otp number for check true or false in backend

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,8 +1,18 @@
 const CheckOtpForm = ({ code, setCode, setStep, mobile }) => {
-  return <form>
-    <p>تایید کد اس ام اس شده</p>
-    <span>کد پیامک شده به شماره «{mobile}» را وارد کنید  </span>
-  </form>;
+  return (
+    <form>
+      <p>تایید کد اس ام اس شده</p>
+      <span>کد پیامک شده به شماره «{mobile}» را وارد کنید </span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+    </form>
+  );
 };
 
 export default CheckOtpForm;
