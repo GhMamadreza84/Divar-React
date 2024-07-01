64cb79b9b7b41c71564c94bcfe84e5e645237330
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-01 01:53:50 -0700

    add checkOtp in auth js for check otp number

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,4 +9,13 @@ const sendOtp = async (mobile) => {
   }
 };
 
+const checkOtp = async (mobile, code) => {
+  try {
+    const response = await api.post("auth/check-otp", { mobile, code });
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
+
 export { sendOtp };
