cf362b02525379e24e33cd4f576067125df4c8aa
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-30 22:56:58 -0700

    edit jsx in check otp component

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,7 +1,7 @@
 const CheckOtpForm = ({ code, setCode, setStep, mobile }) => {
   return <form>
     <p>تایید کد اس ام اس شده</p>
-    <span>کد پیامک شده به شماره را وارد کنید{mobile}</span>
+    <span>کد پیامک شده به شماره «{mobile}» را وارد کنید  </span>
   </form>;
 };
 
