0c89e80e95204917d8ce053d902d80cae8c04e26
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-30 22:55:33 -0700

    add jsx to check otp form

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,10 +1,8 @@
-
-const CheckOtpForm = () => {
-  return (
-    <form>
-      
-    </form>
-  );
-}
+const CheckOtpForm = ({ code, setCode, setStep, mobile }) => {
+  return <form>
+    <p>تایید کد اس ام اس شده</p>
+    <span>کد پیامک شده به شماره را وارد کنید{mobile}</span>
+  </form>;
+};
 
 export default CheckOtpForm;
