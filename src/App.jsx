d01678bf7ff6b1c465aa907f35c0b1769c15335d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 03:02:14 -0700

    create route for auth admin and page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "pages/HomePage";
+import DashboardPage from "pages/DashboardPage";
+import AuthPage from "pages/AuthPage";
+import AdminPage from "pages/AdminPage";
+import PageNotFound from "pages/404";
 
 function App() {
   return (
@@ -7,6 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
