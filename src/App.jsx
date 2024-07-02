25a711f4180373cc1dbf3bee70aaf0a31cb1b01d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 03:00:15 -0700

    add route for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          
+          <Route index element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </>
   );
 }
- 
+
 export default App;
