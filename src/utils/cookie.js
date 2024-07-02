cc122d8b73f1d82feda38565ab29b28098e18c06
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-01 22:27:06 -0700

    get and save acces token and refresh token

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,1 +1,8 @@
- 
+const setToken = (tokens) => {
+  document.cookie = `accesToken = ${tokens.accesToken}; max-age=${
+    1 * 24 * 60 * 60
+  }`;
+  document.cookie = `refreshToken = ${tokens.refreshToken}; max-age=${
+    1 * 24 * 60 * 60
+  }`;
+};
