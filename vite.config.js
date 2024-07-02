ae54fb3908c5eef8464532f71bd5a874aad60e39
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-01 23:07:25 -0700

    edit config for using absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const paths = [
+  "src",
+  "assets",
+  "configs",
+  "components",
+  "pages",
+  "router",
+  "services",
+  "styles",
+  "utils",
+];
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      ...paths.reduce(
+        (acc, curr) => ({
+          ...acc,
+          [curr]: `/${curr === "src" ? curr : "src/" + curr}`,
+        }),
+        ""
+      ),
+    },
+  },
+});
