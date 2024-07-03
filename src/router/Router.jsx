b683c69b4b8b7dbc112860c178beca9b603a7fb0
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 23:32:08 -0700

    import usequery hook in router component

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 import PageNotFound from "src/pages/404";
 import AdminPage from "src/pages/AdminPage";
@@ -6,14 +7,15 @@ import DashboardPage from "src/pages/DashboardPage";
 import HomePage from "src/pages/HomePage";
 
 const Router = () => {
+  const { data, isLoading } = useQuery(["profile"]);
   return (
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 };
 
