ab82077ecf418e8b87f450653033a06dd95ef4b1
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 23:45:48 -0700

    we have bug now

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,9 @@
 import { api } from "src/configs/api";
 import { getCookie } from "utils/cookie";
-console.log(getCookie("accessToken"));
+const token = getCookie("accessToken");
 
-const getProfile = api.get("user/whoami");
+const getProfile = api.get("user/whoami", {
+  headers: { Authorization: `${token}` },
+});
 
 export { getProfile };
