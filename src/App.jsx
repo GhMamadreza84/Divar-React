a0db50a1d2686a170e89b6526de72a32477008cd
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 23:18:46 -0700

    create reactQuery file in config

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,7 @@ import Router from "./router/Router";
 
 function App() {
   const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        retry: 1,
-        staleTime: 60 * 1000,
-      },
-    },
+   
   });
   return (
     <>
