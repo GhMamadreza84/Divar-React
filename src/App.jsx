822ed9be390ee9252052173f7426841b85ae8659
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-02 23:19:47 -0700

    import reacquery  config file and use in app for

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/Router";
+import defaultOptions from "./configs/reactQuery";
 
 function App() {
   const queryClient = new QueryClient({
-   
+    defaultOptions,
   });
   return (
     <>
-      <QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Router />
         </BrowserRouter>
