83d267f3695a1ddca814ec695ffc12d8fb66951d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-03 01:33:07 -0700

    create function get new token in token file

--- src/services/token.js
+++ src/services/token.js
@@ -1,2 +1,12 @@
-const getNewTokens = () => {};
- 
+const { getCookie } = require("src/utils/cookie");
+
+const getNewTokens = async () => {
+  const refreshToken = getCookie("refreshToken");
+  if (!refreshToken) return;
+
+  try {
+    const response = await api.post("auth/check-refresh-token", {
+      refreshToken,
+    });
+  } catch (error) {}
+};
