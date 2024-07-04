a75ecacceaacf32af3d5db2739dd1f41979eaea2
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-04 00:20:11 -0700

    Set user access to enter pages

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { getProfile } from "services/user";
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
@@ -10,13 +10,28 @@ import HomePage from "pages/HomePage";
 const Router = () => {
   const { data, isLoading, error } = useQuery(["profile"], getProfile);
   console.log({ data, isLoading, error });
-  if(isLoading) return <h1>Loading ...</h1>
+  if (isLoading) return <h1>Loading ...</h1>;
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <AuthPage />}
+      />
+      <Route
+        path="/admin"
+        element={
+          data && data.data.role === "ADMIN" ? (
+            <AdminPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
