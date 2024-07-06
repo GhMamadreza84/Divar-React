3c3195d5abce2227940f34b4843400c466ae1778
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 01:13:55 -0700

    add change handler for form in category

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 
 const CategoryForm = () => {
+  const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
   return (
-    <form>
-       <h3>دسته بندی جدید</h3>
-       <label htmlFor="name">اسم دسته بندی</label>
-       <input type="text" name="name" id="name" />
+    <form onChange={changeHandler}>
+      <h3>دسته بندی جدید</h3>
+      <label htmlFor="name">اسم دسته بندی</label>
+      <input type="text" name="name" id="name" />
 
-       <label htmlFor="slug">اسلاگ</label>
-       <input type="text" slug="slug" id="slug" />
+      <label htmlFor="slug">اسلاگ</label>
+      <input type="text" slug="slug" id="slug" />
 
-       <label htmlFor="icon">آیکون</label>
-       <input type="text" name="icon" id="icon" />
+      <label htmlFor="icon">آیکون</label>
+      <input type="text" name="icon" id="icon" />
 
-       <button type="submit">ثبت</button>
+      <button type="submit">ثبت</button>
     </form>
   );
-}
+};
 
 export default CategoryForm;
