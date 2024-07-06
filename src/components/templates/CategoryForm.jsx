4eb39faa05253c408c3385dd4961204b5fd3539f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 01:01:27 -0700

    add another input in category form

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -5,6 +5,12 @@ const CategoryForm = () => {
        <h3>دسته بندی جدید</h3>
        <label htmlFor="name">اسم دسته بندی</label>
        <input type="text" name="name" id="name" />
+
+       <label htmlFor="slug">اسلاگ</label>
+       <input type="text" slug="slug" id="slug" />
+
+       <label htmlFor="icon">آیکون</label>
+       <input type="text" name="icon" id="icon" />
     </form>
   );
 }
