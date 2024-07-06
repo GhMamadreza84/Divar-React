0ea274cb256effd88810f8fe70c700b706488f49
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 00:14:51 -0700

    create right side header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header>
+      <div>
+        <Link to="/">
+          <img src="divar.svg" alt="divar icon" />
+        </Link>
+        <span>
+          <img src="location.svg" alt="location icon" />
+          <p>تهران</p>
+        </span>
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
