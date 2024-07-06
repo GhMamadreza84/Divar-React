1ac68d10a2cb0db560087953fc9d08da23f52159
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 01:00:28 -0700

    add first input for category form

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,9 +1,11 @@
 
 const CategoryForm = () => {
   return (
-    <div>
-      category form
-    </div>
+    <form>
+       <h3>دسته بندی جدید</h3>
+       <label htmlFor="name">اسم دسته بندی</label>
+       <input type="text" name="name" id="name" />
+    </form>
   );
 }
 
