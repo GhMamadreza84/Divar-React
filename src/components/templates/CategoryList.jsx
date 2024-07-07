5ec1606aed2866e728266b76c237718bbf7bdb5c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 23:33:25 -0700

    edit category list jsx

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCategory } from "services/admin";
 import Loader from "../modules/Loader";
-import styles from "./CategoryList.module.css"
+import styles from "./CategoryList.module.css";
 const CategoryList = () => {
   const { data, isLoading } = useQuery(["get-categories"], getCategory);
   console.log({ data, isLoading });
@@ -12,6 +12,7 @@ const CategoryList = () => {
       ) : (
         data.data.map((i) => (
           <div key={i._id}>
+            <img src={`${i.icon}.svg`} alt="" />
             <h5>{i.name}</h5>
             <p>slug : {i.slug}</p>
           </div>
