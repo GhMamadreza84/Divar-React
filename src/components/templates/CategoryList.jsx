5298204bb727c18354568512b5f055ba9bf00d41
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 23:06:49 -0700

    i get data with useQuery and show in ui

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,10 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCategory } from "src/services/admin";
+import { getCategory } from "services/admin";
+import Loader from "../modules/Loader";
 
 const CategoryList = () => {
   const { data, isLoading } = useQuery(["get-categories"], getCategory);
   console.log({ data, isLoading });
-  return <div>category</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.data.map((i) => (
+          <div key={i._id}>
+            <h5>{i.name}</h5>
+            <p>slug : {i.slug}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default CategoryList;
