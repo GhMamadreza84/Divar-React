61a88ece187eb62f141eb0c6d970872d2f7ffeeb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-06 23:03:24 -0700

    i get category in category list component with getcategory

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,11 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "src/services/admin";
 
 const CategoryList = () => {
-  return (
-    <div>
-      CategoryList
-      CategoryList
-    </div>
-  );
-}
+  const { data, isLoading } = useQuery(["get-categories"], getCategory);
+  console.log({ data, isLoading });
+  return <div>category</div>;
+};
 
 export default CategoryList;
