a8364083cb7bbd84ab2f1b165d2d24f5b2de584d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-08 03:44:03 -0700

    i have bug in add post for create select option

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -3,13 +3,13 @@ import { getCategory } from "src/services/admin";
 
 const AddPost = () => {
   const { data } = useQuery(["get-categories"], getCategory);
-
+  // console.log(data.data.name)
   return (
     <form>
       <h3>افزودن اگهی</h3>
       <label htmlFor="title">عنوان</label>
       <input type="text" name="title" id="title" />
-      
+
       <label htmlFor="content">توضیحات</label>
       <textarea name="content" id="content" />
 
@@ -19,7 +19,14 @@ const AddPost = () => {
       <label htmlFor="city">شهر</label>
       <input type="text" name="city" id="city" />
 
-
+      <label htmlFor="category">دسته بندی</label>
+      <select name="category" id="category">
+        {data?.data.map((i) => {
+          <option value={i._id} key={i._id}>
+            {i.name}
+          </option>;
+        })}
+      </select>
     </form>
   );
 };
