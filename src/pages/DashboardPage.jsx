de6b692da85be0b438f209105a9868b4687ada7f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-08 03:34:46 -0700

    i import add post in dashboard page and show in ui

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,10 +1,12 @@
+import AddPost from "src/components/templates/AddPost";
 
 const DashboardPage = () => {
   return (
     <div>
+      <AddPost />
       DashboardPage
     </div>
   );
-}
+};
 
 export default DashboardPage;
