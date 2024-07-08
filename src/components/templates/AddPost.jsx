40e6c469540f5d52e2916274781d74c946d27266
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-08 03:38:05 -0700

    add input and text area to add post

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -1,7 +1,22 @@
 const AddPost = () => {
-  return <form>
-    
-  </form>
+  return (
+    <form>
+      <h3>افزودن اگهی</h3>
+      <label htmlFor="title">عنوان</label>
+      <input type="text" name="title" id="title" />
+      
+      <label htmlFor="content">توضیحات</label>
+      <textarea name="content" id="content" />
+
+      <label htmlFor="amount">قیمت</label>
+      <input type="text" name="amount" id="amount" />
+
+      <label htmlFor="city">شهر</label>
+      <input type="text" name="city" id="city" />
+
+      
+    </form>
+  );
 };
 
 export default AddPost;
