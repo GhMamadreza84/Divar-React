401a60b216d671de21455807686f41129ff51bbb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-10 01:01:08 -0700

    import toast in add post component

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getCategory } from "src/services/admin";
 import styles from "./AddPost.module.css";
 import { getCookie } from "src/utils/cookie";
+import toast from "react-hot-toast";
 const AddPost = () => {
   const { data } = useQuery(["get-categories"], getCategory);
   const [form, setForm] = useState({
