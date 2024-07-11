b7a6e96bd020f0292d23a73b91da3be1cfdb0654
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-10 23:36:05 -0700

    create css file for post list and add class name to component postlist

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -2,13 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getPosts } from "services/user";
 import Loader from "../modules/Loader";
 import { sp } from "utils/numbers";
-
+import styles from "./PostList.module.css"
 const PostList = () => {
   const { data, isLoading } = useQuery(["my-post-list"], getPosts);
   console.log({ data, isLoading });
   const baseUrl = import.meta.env.VITE_BASE_URL;
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
@@ -16,13 +16,13 @@ const PostList = () => {
           <br />
           <h3>آگهی های شما</h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${baseUrl}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
+              <div className={styles.price}>
                 <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
                 <span>{sp(post.amount)} تومان</span>
               </div>
