3ec190caa5e0ba20e392be35cb64c2759b713db1
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 23:27:47 -0700

    i get all post in home page with useQuery and ccreate function in user for send get request to api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import Main from "src/components/templates/Main";
 import SideBar from "src/components/templates/SideBar";
+import { getAllPosts } from "src/services/user";
 
 const style = { display: "flex" };
 
 const HomePage = () => {
+  const { data , isLoading} = useQuery(["post-list"], getAllPosts);
+
   return (
     <div style={style}>
       <SideBar />
