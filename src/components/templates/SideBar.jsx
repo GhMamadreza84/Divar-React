d213dd1f6093f36091f738e28994054427c87423
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 23:06:27 -0700

    i map on the category data and create li for category and show in sidebar component

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,10 +1,17 @@
 const SideBar = () => {
   const { data } = useQuery(["get-categories"], getCategory);
 
-  return <div>
-    <h4>دسته ها </h4>
-    
-  </div>;
+  return (
+    <div>
+      <h4>دسته ها </h4>
+      {data?.data.map((category) => (
+        <li key={category._id}>
+          <img src={`${category.icon}.svg`} alt="side-bar-icon" />
+          <p>{category.name}</p>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default SideBar;
