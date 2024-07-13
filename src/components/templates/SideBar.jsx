1541e4eef1525c38b43df5b9c4e18e80ebe35ce8
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 23:04:40 -0700

    i get category in side bar with useQuery

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,10 +1,10 @@
-
 const SideBar = () => {
-  return (
-    <div>
-      sidebar
-    </div>
-  );
-}
+  const { data } = useQuery(["get-categories"], getCategory);
+
+  return <div>
+    <h4>دسته ها </h4>
+    
+  </div>;
+};
 
 export default SideBar;
