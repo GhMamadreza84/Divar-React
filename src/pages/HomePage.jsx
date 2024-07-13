196d03ebd65ec261249e3e8f0defba25b5da85d0
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 22:59:47 -0700

    add inline style in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import Main from "src/components/templates/Main";
 import SideBar from "src/components/templates/SideBar";
 
+const style = { display: "flex" };
+
 const HomePage = () => {
   return (
-    <div>
+    <div style={style}>
       <SideBar />
       <Main />
     </div>
   );
-}
+};
 
 export default HomePage;
