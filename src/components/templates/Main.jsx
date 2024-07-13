db9a172c2df4b2d6ba4d2ca3254fe34ab9b2d360
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 23:36:52 -0700

    edit homepage and main component

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,10 +1,12 @@
-
-const Main = () => {
-  return (
-    <div>
-      main
-    </div>
-  );
-}
+const Main = ({ data }) => {
+  return <div>
+    {data?.data.posts.map(post=>(
+      <div>
+        <img src={post.images} alt="" />
+        <p>{post.name}</p>
+      </div>
+    ))}
+  </div>;
+};
 
 export default Main;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import Main from "src/components/templates/Main";
-import SideBar from "src/components/templates/SideBar";
-import { getAllPosts } from "src/services/user";
+import Main from "components/templates/Main";
+import SideBar from "components/templates/SideBar";
+import { getAllPosts } from "services/user";
 
 const style = { display: "flex" };
 
 const HomePage = () => {
-  const { data , isLoading} = useQuery(["post-list"], getAllPosts);
-  console.log({data,isLoading})
+  const { data } = useQuery(["post-list"], getAllPosts);
+  console.log({data})
   return (
     <div style={style}>
       <SideBar />
-      <Main />
+      <Main data={data}  />
     </div>
   );
 };
