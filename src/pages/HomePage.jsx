b150731128429d96c3e61be7be127966d30f3a32
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-07-12 23:37:44 -0700

    add loading to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import Main from "components/templates/Main";
 import SideBar from "components/templates/SideBar";
 import { getAllPosts } from "services/user";
+import Loader from "components/modules/Loader";
 
 const style = { display: "flex" };
 
 const HomePage = () => {
-  const { data } = useQuery(["post-list"], getAllPosts);
-  console.log({data})
+  const { data, isLoading } = useQuery(["post-list"], getAllPosts);
+  console.log({ data });
   return (
-    <div style={style}>
-      <SideBar />
-      <Main data={data}  />
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div style={style}>
+          <SideBar />
+          <Main data={data} />
+        </div>
+      )}
+    </>
   );
 };
 
