85d332f24bf2aca3171b8f9f1ca789335701d735
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-16 20:02:19 +0430

    i remove all files and start from first project

--- src/services/admin.js
+++
@@ -1,7 +0,0 @@
-import { api } from "configs/api";
-
-const addCategory = (data) => api.post("category", data);
-
-const getCategory = () => api.get("category" );
-
-export { addCategory,getCategory };
--- src/services/user.js
+++
@@ -1,9 +0,0 @@
-import { api } from "src/configs/api";
-
-const getProfile = () => api.get("user/whoami").then((res) => res || false);
-
-const getPosts = () => api.get("post/my");
-
-const getAllPosts = () => api.get("");
-
-export { getProfile, getPosts , getAllPosts };
