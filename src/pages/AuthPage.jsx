dee923ae83273c1f1ccc2a2c893adb9c8136c2a7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-17 20:07:45 +0430

    i define mobile , code state in auth page for handle login and signup operation

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,7 +3,9 @@ import SendOtpForm from "../components/templates/SendOtpForm";
 import CheckOtpForm from "../components/templates/CheckOtpForm";
 
 const AuthPage = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
   return <div>{step === 1 ? <SendOtpForm /> : <CheckOtpForm />}</div>;
 };
 
