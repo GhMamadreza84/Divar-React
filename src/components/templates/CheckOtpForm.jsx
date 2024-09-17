4ad76469d6d6951d652c8e96cc7192d525298317
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-17 20:01:52 +0430

    i create a template folder in component and in template i create check otp and send otp form

---
+++ src/components/templates/CheckOtpForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckOtpForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckOtpForm
---
+++ src/components/templates/SendOtpForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SendOtpForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SendOtpForm
