c13181ce04c2525eb2d662eeac8fac4f0b27ee91
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-17 20:14:39 +0430

    i write the jsx files sendOtpForm

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const SendOtpForm = () => {
   return (
-   <form >
-    <p>ورود به حساب کاربری</p>
-    <span>برای استفاده از امکانات دیوار , لطفا شماره موبایل خود را وارد کنید . کد تایید به این شماره پیامک خواهد شد.</span>
-    <label htmlFor="input">شماره</label>
-   </form>
-  )
-}
+    <form>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار , لطفا شماره موبایل خود را وارد کنید . کد
+        تایید به این شماره پیامک خواهد شد.
+      </span>
+      <label htmlFor="input">شماره موبایل خود را وارد کنید </label>
+      <input
+        type="text"
+        id="input"
+        placeholder="شماره موبایل"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <button type="submit">ارسال کد تایید</button>
+    </form>
+  );
+};
 
-export default SendOtpForm
+export default SendOtpForm;
