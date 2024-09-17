c84fd1cf3b4ca4a07b2c012267a2de2e639a5728
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-17 19:52:29 +0430

    i now define the vitebaseurl in api base url

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3400/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
