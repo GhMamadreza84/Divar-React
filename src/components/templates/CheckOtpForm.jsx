bf8ed1f1b2b07ea59a405fe962876132090493ff
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-20 00:11:07 +0430

    i complate the jsx ui for check otp form

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -4,7 +4,17 @@ const CheckOtpForm = ({ mobile, code, setCode, setStep }) => {
   return (
     <form>
       <p>تایید کد اس ام اس شده </p>
-        
+      <span>کد پیامک شده به شماره موبایل {mobile} را وارد کنید.</span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
     </form>
   );
 };
