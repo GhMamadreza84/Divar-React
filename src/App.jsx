4bde093a7a95a0d42587fb57493e992600e9cb48
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 19:57:01 +0330

    i set the setting for queryClient

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import Router from "./router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: 1,
+        staleTime: 60 * 1000,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
-    </QueryClientProvider >
+    </QueryClientProvider>
   );
 }
 
