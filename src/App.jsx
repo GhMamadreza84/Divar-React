4a9accac99e6cf6a8d10a115ff0419db314380bb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 19:59:06 +0330

    i now import defualt option from queryclient and use it in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import defaultOptions from "./configs/queryClient";
 
 function App() {
   const queryClient = new QueryClient({
-  
+    defaultOptions : defaultOptions
   });
   return (
     <QueryClientProvider client={queryClient}>
