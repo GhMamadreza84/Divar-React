0ad51172e99c1a9a6b3ee71a219c564aa1d191ec
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 19:54:34 +0330

    i wrap the query client provider rounded the app and now go set default setting for queryClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider >
   );
 }
 
