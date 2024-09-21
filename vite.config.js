9c99b97a366cc534235ffa28b47e0c797cb2af0c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 10:17:21 +0330

    i added a array paths in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const paths = [
+  "src",
+  "assets",
+  "components",
+  "configs",
+  "pages",
+  "router",
+  "services",
+  "styles",
+  "utils",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
