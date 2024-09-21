dd6f090b445fa4ab24ff27ce87dd9e04c903ccf2
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 09:45:23 +0330

    i add refresh token to setCookie and this refershToken expire after thirty day

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,5 +1,8 @@
-const setCookie = (token) => {
-  document.cookie = `accessToken=${token.accessToken}; max-age=${
+const setCookie = (tokens) => {
+  document.cookie = `accessToken=${tokens.accessToken}; max-age=${
     1 * 24 * 60 * 60
+  }`;
+  document.cookie = `refreshToken=${tokens.refreshToken}; max-age=${
+    30 * 24 * 60 * 60
   }`;
 };
