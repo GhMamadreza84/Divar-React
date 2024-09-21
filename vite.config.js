19d402fec463568534b21bfac7183fdffe795386
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 10:22:09 +0330

    i performed the reduce on the paths in vite config

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,12 @@ const paths = [
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ...paths.reduce((acc, cur) => ({
+        ...acc,
+        [cur]: `/${cur === "src" ? cur : "src/" + cur} `,
+      })),
+    },
+  },
 });
