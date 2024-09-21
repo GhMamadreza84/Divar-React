6c93653037c95a78e6b325328c21d6fe985218d4
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-21 20:00:15 +0330

    i import reactquery devtools and use it in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
-import Router from "./router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Router from "./router/Router";
 import defaultOptions from "./configs/queryClient";
 
 function App() {
   const queryClient = new QueryClient({
-    defaultOptions : defaultOptions
+    defaultOptions,
   });
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
