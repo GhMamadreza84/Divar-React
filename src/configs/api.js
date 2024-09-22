c52c359932bdc5ae04e0472f298d5efd47b0cd07
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-22 19:04:57 +0330

    i move the operation set the token in header to api file

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getCookie } from "../utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -7,4 +8,17 @@ const api = axios.create({
   },
 });
 
-export default api; 
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
