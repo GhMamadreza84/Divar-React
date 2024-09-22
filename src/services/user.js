c73772874311b57aaa3f1769e18273024ed4f000
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-22 18:59:12 +0330

    i wanna move set headers token to api

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,7 @@
 import api from "../configs/api";
 import { getCookie } from "../utils/cookie";
 
-const token = getCookie("accessToken");
-
 const getProfile = () =>
-  api.get("user/whoami", { headers: { Authorization: `bearer ${token}` } });
+  api.get("user/whoami");
 
 export { getProfile };
