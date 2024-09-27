8c20bc15ff348bd574e2507118337655ca881557
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 18:36:54 +0330

    i add class name and add styles to header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,24 +3,26 @@ import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header>
-      <div className="">
+    <header className={styles.header}>
+      <div>
         <Link to="/">
-          <img src="divar.svg" alt="" />
+          <img className={styles.logo} src="divar.svg" alt="" />
         </Link>
         <span>
           <img src="location.svg" alt="" />
           <p>تهران</p>
         </span>
-        <div className="">
-          <Link to="/auth">
-            <span>
-              <img src="profile.svg" alt="" />
-              <p>دیوار من</p>
-            </span>
-          </Link>
-          <Link to="/dashboard">ثبت آگهی</Link>
-        </div>
+      </div>
+      <div className="">
+        <Link to="/auth">
+          <span>
+            <img src="profile.svg" alt="" />
+            <p>دیوار من</p>
+          </span>
+        </Link>
+        <Link className={styles.button} to="/dashboard">
+          ثبت آگهی
+        </Link>
       </div>
     </header>
   );
