40cc03a39ae49b45e29a297cd94d7bc2fcd89edf
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 06:50:17 +0330

    Now I want to specify the user's access level in the project

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { useNavigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import DashboardPage from "../pages/DashboardPage";
 import PageNotFount from "../pages/404";
@@ -11,15 +11,21 @@ import { getNewTokens } from "../configs/tokens";
 
 const Router = () => {
   const { data, isLoading, error } = useQuery(["profile"], getProfile);
-  console.log({data,isLoading})
+  console.log({ data, isLoading });
   // if (isLoading) return <h1>Loading...</h1>;
-
+  const role = data?.data?.role;
+  console.log(role);
+  const navigate = useNavigate()
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      {role === "ADMIN" ? (
+        <Route path="/admin" element={<AdminPage />} />
+      ) : (
+        <Navigate path="/admin" to={<DashboardPage />} />
+      )}
       <Route path="*" element={<PageNotFount />} />
     </Routes>
   );
