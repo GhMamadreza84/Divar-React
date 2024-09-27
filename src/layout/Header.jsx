9245f71480a91af23e45d1d3e300862f4f19b25f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 18:26:16 +0330

    i now complate the header jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div>
-      header
-      
-    </div>
-  )
-}
+    <header>
+      <div className="">
+        <Link to="/">
+          <img src="divar.svg" alt="" />
+        </Link>
+        <span>
+          <img src="location.svg" alt="" />
+          <p>تهران</p>
+        </span>
+        <div className="">
+          <Link to="/auth">
+            <span>
+              <img src="profile.svg" alt="" />
+              <p>دیوار من</p>
+            </span>
+          </Link>
+          <Link to="/dashboard">ثبت آگهی</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
