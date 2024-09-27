a00a323da7a216ac65104824ec0ce22a287cee77
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 18:30:06 +0330

    i add class name and css to layout file

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from "./Layout.module.css"
+import styles from "./Layout.module.css";
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <div className={styles.main}>{children}</div>
       <Footer />
     </>
   );
