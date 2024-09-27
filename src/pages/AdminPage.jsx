15be747edac57d8842716a4c633dd0650281308d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 20:14:05 +0330

    i now create the inputs in admin page and admin can create new category now

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,17 +1,51 @@
 import React, { useState } from "react";
+import { addCateGory, getCategory } from "../services/category";
 
 const AdminPage = () => {
   const [name, setName] = useState("");
   const [slug, setSlug] = useState("");
   const [icon, setIcon] = useState("");
-  const [parent, setParent] = useState("");
+  // const [parent, setParent] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
+    const categoryData = {
+      name,
+      slug,
+      icon,
+      // parent,
+    };
+    try {
+      const result = await addCateGory(categoryData);
+      console.log("Category added", result);
+    } catch (error) {
+      console.error("Error adding category:", error.message);
+    }
   };
 
-  return <div>AdminPage AdminPage</div>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="icon"
+          onChange={(e) => setIcon(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="slug"
+          onChange={(e) => setSlug(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/services/category.js
+++ src/services/category.js
@@ -2,9 +2,22 @@ import api from "../configs/api";
 
 const addCateGory = async (categoryData) => {
   try {
-    const response = await api.post("/category");
+    const response = await api.post("/category", categoryData);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Error adding category");
   }
 };
+
+const getCategory = async () => {
+  try {
+    const response = await api.get("/category");
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Error fetching categories"
+    );
+  }
+};
+
+export { addCateGory, getCategory };
