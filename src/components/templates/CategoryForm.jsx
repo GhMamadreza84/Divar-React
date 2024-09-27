2e8d473dbfdc3d1786f5e35712b048332d2839c3
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 21:01:01 +0330

    i now handle error and result for add category with tost

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { addCateGory } from "../../services/category";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import styles from "./CategoryForm.module.css";
 
 const CategoryForm = () => {
   const [name, setName] = useState("");
   const [slug, setSlug] = useState("");
   const [icon, setIcon] = useState("");
-  const [error, setError] = useState("");
-  const [result, setResult] = useState("");
+
   // const [parent, setParent] = useState("");
 
   const handleSubmit = async (e) => {
@@ -21,47 +22,48 @@ const CategoryForm = () => {
     };
     try {
       const result = await addCateGory(categoryData);
-      console.log("category added", result);
-      setResult("دسته بندی با موفقیت اضافه شد ");
+      toast.success("دسته‌بندی با موفقیت اضافه شد!");
     } catch (error) {
-      setError(error.message);
+      toast.error("خطایی رخ داد. لطفاً دوباره تلاش کنید.");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <h3>دسته بندی جدید</h3>
-      {error ? <p>لطفا با دقت مقادیر رو وارد کنید</p> : null}
-      {result ? <p>{result}</p> : null}
-      {/* name */}
-      <label htmlFor="name">اسم دسته بندی</label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        placeholder="name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      {/* slug */}
-      <label htmlFor="slug">آیکن </label>
-      <input
-        id="slug"
-        type="text"
-        name="slug"
-        placeholder="icon"
-        onChange={(e) => setIcon(e.target.value)}
-      />
-      {/* icon */}
-      <label htmlFor="icon">اسلاگ</label>
-      <input
-        id="icon"
-        type="text"
-        name="icon"
-        placeholder="slug"
-        onChange={(e) => setSlug(e.target.value)}
-      />
-      <button type="submit">ایجاد</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h3>دسته بندی جدید</h3>
+
+        {/* name */}
+        <label htmlFor="name">اسم دسته بندی</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        {/* slug */}
+        <label htmlFor="slug">آیکن </label>
+        <input
+          id="slug"
+          type="text"
+          name="slug"
+          placeholder="icon"
+          onChange={(e) => setIcon(e.target.value)}
+        />
+        {/* icon */}
+        <label htmlFor="icon">اسلاگ</label>
+        <input
+          id="icon"
+          type="text"
+          name="icon"
+          placeholder="slug"
+          onChange={(e) => setSlug(e.target.value)}
+        />
+        <button type="submit">ایجاد</button>
+      </form>
+      <ToastContainer position="top-right" autoClose={5000} />
+    </div>
   );
 };
 
