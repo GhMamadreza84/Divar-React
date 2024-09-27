0810ad92185c6645c46bad827d3448fb4e852245
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 18:29:14 +0330

    i import css file in layouts file

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import styles from "./Layout.module.css"
 const Layout = ({ children }) => {
   return (
     <>
