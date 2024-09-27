b24a8b58f50d764eaba1eded7e87b2928ff01f0e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-27 21:15:21 +0330

    i show category details now in ui admin panel

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -44,8 +44,12 @@ const CategoryForm = () => {
     <div>
       <div>
         {categories.length > 0 ? (
-          categories.map((category, index) => (
-            <div key={index}>{category.name}</div>
+          categories.map((category) => (
+            <div key={category._id}>
+              <img src={`${category.icon}.svg`} alt="" />
+              <h5>{category.name}</h5>
+              <p>slug: {category.slug}</p>
+            </div>
           ))
         ) : (
           <p>دسته بندی ای وجود ندارد</p>
