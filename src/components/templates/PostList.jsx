c5b03a9c5e7f454e037210201fa5a3e9270fdce0
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-04 21:58:02 +0330

    i now create a postlist css file and import in postlist component for add styles to this

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -2,27 +2,28 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getPosts } from "../../services/user";
 import Loader from "../modules/Loader";
-
+import { sp } from "../../utils/numbers";
+import styles from "./PostList.module.css";
 const PostList = () => {
   const { data, isLoading } = useQuery(["my-post-list"], getPosts);
   console.log({ data, isLoading });
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
         <>
           <h3>آگهی های شما</h3>
           {data?.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
+              <div className={styles.price}>
                 <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
-                <span>{post.amount}تومان</span>
+                <span>{sp(post.amount)} تومان</span>
               </div>
             </div>
           ))}
