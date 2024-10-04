d8f25cd66564669942b300770aa742590f0d6e11
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-04 21:15:30 +0330

    i complate the addpost component

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -5,6 +5,9 @@ import { addPost } from "../../services/post";
 import styles from "./AddPost.module.css";
 import { getCookie } from "../../utils/cookie";
 import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+
 const AddPost = () => {
   const { data } = useQuery(["category"], getCategory);
   const [form, setForm] = useState({
@@ -36,33 +39,36 @@ const AddPost = () => {
           Authorization: `bearer ${token}`,
         },
       })
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
+      .then(() => toast.success("آگهی با موفقیت منتشر شد"))
+      .catch(() => toast.error("بنظر مشکلی وجود داره عزیزم"));
   };
   return (
-    <form onChange={changeHandler} className={styles.form}>
-      <h3>افزودن آگهی</h3>
-      <label htmlFor="title">عنوان</label>
-      <input type="text" name="title" id="title" />
-      <label htmlFor="content">توضیحات</label>
-      <textarea name="content" id="content"></textarea>
-      <label htmlFor="amount">قیمت</label>
-      <input type="text" name="amount" id="amount" />
-      <label htmlFor="city">شهر</label>
-      <input type="text" name="city" id="city" />
-      <label htmlFor="category">دسته بندی</label>
-      <select name="category" id="category">
-        <option>دسته بندی مورد نظر را انتخاب کنید</option>
-        {data?.map((i) => (
-          <option key={i._id} value={i._id}>
-            {i.name}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="images">عکس</label>
-      <input type="file" name="images" id="images" />
-      <button onClick={addHandler}>ثبت آگهی</button>
-    </form>
+    <>
+      <form onChange={changeHandler} className={styles.form}>
+        <h3>افزودن آگهی</h3>
+        <label htmlFor="title">عنوان</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="content">توضیحات</label>
+        <textarea name="content" id="content"></textarea>
+        <label htmlFor="amount">قیمت</label>
+        <input type="text" name="amount" id="amount" />
+        <label htmlFor="city">شهر</label>
+        <input type="text" name="city" id="city" />
+        <label htmlFor="category">دسته بندی</label>
+        <select name="category" id="category">
+          <option>دسته بندی مورد نظر را انتخاب کنید</option>
+          {data?.map((i) => (
+            <option key={i._id} value={i._id}>
+              {i.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="images">عکس</label>
+        <input type="file" name="images" id="images" />
+        <button onClick={addHandler}>ثبت آگهی</button>
+      </form>
+      <ToastContainer position="top-right" autoClose={5000} />
+    </>
   );
 };
 
