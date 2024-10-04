7c80aa44f22337a54f6db73055b1f0090c5b0707
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-04 21:38:40 +0330

    i now show posts in post list in dashboard page we need now add a style to postlist component

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -6,7 +6,30 @@ import Loader from "../modules/Loader";
 const PostList = () => {
   const { data, isLoading } = useQuery(["my-post-list"], getPosts);
   console.log({ data, isLoading });
-  return <div>{isLoading ? <Loader /> : <h3></h3>}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <h3>آگهی های شما</h3>
+          {data?.data.posts.map((post) => (
+            <div key={post._id}>
+              <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
+              <div>
+                <p>{post.options.title}</p>
+                <span>{post.options.content}</span>
+              </div>
+              <div>
+                <p>{post.options.createdAt}</p>
+                <span>{post.amount}تومان</span>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default PostList;
