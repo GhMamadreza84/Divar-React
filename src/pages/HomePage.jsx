bcb6fd1eac6dfe524c5cd430a64055621170d969
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-05 10:47:49 +0330

    i add style for homepage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Sidebar from "../components/templates/Sidebar";
 import Main from "../components/templates/Main";
 
+const style = { display: "flex", justifyContent: "space-between" };
+
 const HomePage = () => {
   return (
-    <div>
+    <div style={style}>
       <Sidebar />
       <Main />
     </div>
