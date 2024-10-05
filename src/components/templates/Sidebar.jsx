8e83e4d72d689d3ca8cfa31a1dfd816c9017eb90
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-05 11:49:10 +0330

    i wanna show categories in homepage ui

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { getCategory } from "../../services/category";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { data, isLoading } = useQuery(["category"], getCategory);
+  console.log({ data, isLoading });
+  return (
+    <div>
+      <h3>دسته بندی ها</h3>
+      {isLoading ? (
+        <p>در حال بارگذاری</p>
+      ) : (
+        data?.map((category) => (
+          <div >
+            <img src={`${category.icon}.svg`} alt="" />
+            <span>{category.name}</span>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
