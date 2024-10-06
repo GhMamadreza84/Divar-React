9272ea617c22a7f3c7cdbff8c01c9574d18c7777
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-06 09:00:02 +0330

    i edit the main component

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { sp } from "../../utils/numbers";
 
 const Main = ({ data, isLoading }) => {
   if (isLoading) return <p>درحال بارگزاری</p>;
@@ -6,8 +7,15 @@ const Main = ({ data, isLoading }) => {
   return (
     <div>
       {data?.data?.posts.map((post) => (
-        <div>
-          <img src={post.images[0]} alt="" />
+        <div key={post._id}>
+          <div>
+            <p>{post.options.title}</p>
+            <div>
+              <p>{sp(post.amount)} تومان</p>
+              <span>{post.options.city}</span>
+            </div>
+          </div>
+          <img src="" alt="" />
         </div>
       ))}
     </div>
